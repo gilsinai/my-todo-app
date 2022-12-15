cdcd33af22bf3389e44840dcbc449f307c9c83a7
gilsinai <gilsinai401@gmail.com>
2022-12-15 08:27:41 +0200

    prepare env to production

--- web.py
+++ web.py
@@ -18,10 +18,9 @@ st.subheader("This is my ToDo list application.")
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
-        print(checkbox)
         todos.pop(index)
         functions.write_todos(todos)
-        del st.sesstion_state[todo]
+        del st.session_state[todo]
         st.experimental_rerun()
 
 

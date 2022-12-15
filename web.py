027c877be171c33099362ef37b5cb8be1ec6dda6
gilsinai <gilsinai401@gmail.com>
2022-12-15 10:45:19 +0200

    venv fixes

--- web.py
+++ web.py
@@ -27,4 +27,4 @@ for index, todo in enumerate(todos):
 st.text_input(label="Enter a todo:", placeholder="A new todo...",
               on_change=add_todo, key="new_todo")
 
-st.session_state
+# st.session_state
